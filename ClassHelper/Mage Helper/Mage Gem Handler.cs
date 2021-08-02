8c54dd99cec32fa19d4200830e2990806b7342f9
TheSmokie <jaywalls21@gmail.com>
2021-08-01 23:44:02 -0600

    Added Mage folder / file (08/01/2012)

---
+++ ClassHelper/Mage Helper/Mage Gem Handler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHelper.Mage_Helper
+{
+    class Mage_Gem_Handler
+    {
+    }
+}
